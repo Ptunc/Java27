1d27d958fc4ee8bfc5300c6c63ccc627f744611d
Ptunc <spolentunc@gmail.com>
2022-10-22 11:46:16 +0300

    just commit

---
+++ java27.java
@@ -0,0 +1,27 @@
+package java27;
+
+import java.util.Scanner;
+
+public class java27 {
+    public static void main(String[] args) {
+        int a;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("a: ");
+        a = input.nextInt();
+
+        for(int i=0; i<=a; i++){
+            for(int j=a; j>=a-i; j--){
+                System.out.print(" ");
+            }
+            for(int k=0; k<=a-i; k++)
+                System.out.print("*");
+            for(int z=1; z<=a-i; z++)
+                System.out.print("*");
+            System.out.println("");
+        }
+
+    }
+        
+}
+
